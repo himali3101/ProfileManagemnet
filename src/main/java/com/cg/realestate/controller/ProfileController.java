8dd1ad1fd93c83d1d535a1abc46ba1fb5444a411
Himali Gunjal <himaligunjal31@gmail.com>
2020-05-06 17:08:47 +0530

    commit

---
+++ src/main/java/com/cg/realestate/controller/ProfileController.java
@@ -0,0 +1,56 @@
+package com.cg.realestate.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cg.realestate.model.User;
+import com.cg.realestate.service.ProfileService;
+
+@RestController
+@CrossOrigin
+public class ProfileController {
+	
+	@Autowired
+	ProfileService service;
+	
+	@GetMapping(path = "/")
+	public List<User> display() {
+		
+		return service.findAll();
+		//return "display";
+	}
+	
+
+	@PostMapping(path = "/create")
+	public User createProfile(@RequestBody User user) {
+		
+		return service.createProfile(user);
+		
+	}
+	
+	@PostMapping(path = "/update")
+	public String updateProfile(@RequestBody User updateUser) {
+		
+		User user = service.updateProfile(updateUser);		
+		if(user == null) {
+			return "not updated";
+		}
+		else {
+			return "Profile Updated";
+		
+		}	
+	}
+	
+	/*@GetMapping(path = "/login/{emailId}")
+	public User (@PathVariable String emailId) {
+		return service.findByEmailId(emailId);
+	}
+	*/
+}
---
+++ src/main/java/com/cg/realestate/service/ProfileService.java
@@ -0,0 +1,52 @@
+package com.cg.realestate.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.cg.realestate.dao.ProfileDao;
+import com.cg.realestate.model.User;
+
+@Service
+public class ProfileService{
+
+	@Autowired
+	private ProfileDao dao;
+	
+	public User createProfile(User adduser) {
+		User user = dao.save(adduser);
+		return user;
+	}
+	
+	public List<User> findAll(){
+		 List<User> users = new ArrayList<>();
+		 dao.findAll().forEach(users::add);
+		 return users;
+		
+	}
+	
+	public User updateProfile(User updateUser) {
+	
+		User user = dao.findById(updateUser.getUserId()).get();
+	 
+		if( user == null) {
+	    	return null;
+	    }
+	    else {
+	    	user.setUserName(updateUser.getUserName());
+	    	user.setEmailId(updateUser.getEmailId());
+	    	user.setPassword(updateUser.getPassword());
+	    	user.setPhoneNo(updateUser.getPhoneNo());
+	    	user.setUserType(updateUser.getUserType());
+	    	
+	    	dao.save(user);
+	    	return user;
+	    }
+	}
+	
+	/*public User findByEmailId(String emailId) {
+		return dao.findByEmailId(emailId)
+	}*/
+}
